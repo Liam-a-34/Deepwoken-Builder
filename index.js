75c3e26a1f987b9800223cc938fe4490719b14ba
Liam Allen <liamallen343@gmail.com>
2023-01-19 11:56:13 -0600

    Added random stat rolling

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
+var statTotal = 0
+var chosenArr = []
 
-const statsArr = [
-    [0, 1, 2, 3, 4, 5],
-    [0, 1, 2, 3]
-]
-const statTotal = []
+$("#roll-btn").on("click", () => {
+
+    statTotal = 0
+
+    chosenArr = []
+
+
+    $("#weapon-stats-numbs").children().html("0")
+    $("#main-stats-numbs").children().html("0")
+    $("#mantra-stats-numbs").children().html("0")
+
 
 let randomWeapon = Math.floor(Math.random() * 3)
 
 let randomStat = Math.floor(Math.random() * 101)
 
-statTotal.push(randomStat)
+    statTotal += randomStat;
 
-$("#weapon-stats-numbs").children(randomWeapon).val(randomStat)
+    console.log(statTotal)
+
+$(`#weapon-${randomWeapon}`).html(randomStat)
+
+
+    console.log("weapon working?")
+
+    genStats()
+
+    return statTotal;
+})
+
+
+function genStats(){
+
+    let randomCat = Math.floor(Math.random() * 10)
+
+    let randomStat = Math.floor(Math.random() * 101)
+
+    if(chosenArr.includes(randomCat)){
+        console.log("matched")
+       return genStats();
+    }
+        console.log("No match")
+        chosenArr.push(randomCat)
+
+        if((statTotal + randomStat) >= 327){
+            let remainder = 327 - statTotal;
+            $(`#stat-${randomCat}`).html(remainder);
+            console.log(`remainder in ${randomCat}`)
+            console.log(statTotal + remainder)
+            return;
+        } else {
+            statTotal += randomStat;
+            $(`#stat-${randomCat}`).html(randomStat)
+            console.log(`${randomCat}`)
+            console.log(statTotal)
+            return genStats();
+        }
+    
+    }
